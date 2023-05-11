c9425240cfe46663e11401e94727b587826c1e50
mxd7xx <131319267+mxd7xx@users.noreply.github.com>
2023-05-11 11:23:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['201129491146'] //ur owner number
+global.ownername = "𝘿 َِ𝘼 َِ𝙍 َِ𝙆 ِ۬𝙓 𖤐 ↯" //ur owner name
+global.ytname = "YT:" //ur yt chanel name
+global.socialm = "INSTA: @r8_2z" //ur github or insta name
+global.location = "Africa, Egypt, Minya" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
-global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.ownernomer = "201129491146" //ur number
+global.premium = ['201129491146'] //ur premium number
+global.botname = '𝘿 َِ𝘼 َِ𝙍 َِ𝙆 ِ۬𝙓 َ𝘽 َ𝙊 َْ𝙏 𖤐 ↯' //ur bot name
+global.linkz = "." //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/3b5dd8eaefa86bde71429.jpg" //ur website to be displayed
+global.botscript = '.' //script link
+global.themeemoji = "✔" //ur theme emoji
+global.packname = "𝘿 َِ𝘼 َِ𝙍 َِ𝙆 ِ۬𝙓 َ𝘽 َ𝙊 َْ𝙏 𖤐 ↯" //ur sticker watermark packname
+global.author = "+201557241739" //ur sticker watermark author
+global.wm = "𝘿 َِ𝘼 َِ𝙍 َِ𝙆 ِ۬𝙓 َ𝘽 َ𝙊 َْ𝙏 𖤐 ↯" //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,7 +40,7 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
+    success: 'تم ✨',
     admin: '『 الامر دا لادمن بس! 』',
     botAdmin: '『 ارفع البوت ادمن الاول 』',
     premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
